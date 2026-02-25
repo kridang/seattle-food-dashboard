aeb98a61f2397b78fc948f2be1bd8637a07a4754
kristy <dangnikristy@gmail.com>
2026-02-25 12:12:43 -0800

    kridang: main.js update

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// mapboxgl.accessToken = 
+
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-122.3321, 47.6062],
+    zoom: 12
+});
+
+// initial declarations
