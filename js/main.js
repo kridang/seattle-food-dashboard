a87c0d4e08ce83e21df3e6e744ae826d7f3e0dc6
kristy <dangnikristy@gmail.com>
2026-02-25 17:06:09 -0800

    kridang: contact slideshow (unfinished), favicon

--- js/main.js
+++ js/main.js
@@ -8,3 +8,37 @@ let map = new mapboxgl.Map({
 });
 
 // initial declarations
+
+// contact
+function plusSlide(num) {
+    currentSlide += num;
+    
+    if (currentSlide > totalSlides) {
+    currentSlide = 1;
+    } else if (currentSlide < 1) {
+    currentSlide = totalSlides;
+    }
+    
+    showSlide(currentSlide);
+}
+
+function goToSlide(slideNumber) {
+    showSlide(slideNumber);
+}
+
+function showSlide(slideNumber) {
+    const slides = document.getElementsByClassName('slide');
+    const dots = document.getElementsByClassName('dot');
+    
+    for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove('active');
+    }
+    
+    for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('active');
+    }
+    
+    slides[slideNumber - 1].classList.add('active');
+    dots[slideNumber - 1].classList.add('active');
+    document.getElementById('current-slide').textContent = slideNumber;
+}
